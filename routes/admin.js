42917366d8599e4373be1c7d52a322d8391bb432
Belgotech <Belovedobiora2002@gmail.com>
2022-09-29 19:14:30 -0700

    fixed some synstax errors

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,20 @@ const path = require('path');
 const rootDir = require("../util/util")
 
 const router = express.Router();
+const products = []
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'client', 'add-product.html') )
   // res.sendFile(path.join(__dirname, '../', 'client', 'add-product.html') )
   });
 
+// /admin/add-product => POST  
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({title: req.body.title})
   res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router
+exports.products = products

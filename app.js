351d7e4568d9861aecf3bbb6605baadd205e9062
Belgotech <Belovedobiora2002@gmail.com>
2022-10-02 22:41:16 -0700

    runing

--- app.js
+++ app.js
@@ -21,4 +21,5 @@ app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, './', 'views', 'error.html'))
 });
 
-app.listen(3000);
+app.listen(3000)
+console.log('server is runing');
